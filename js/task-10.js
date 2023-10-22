e0d9c1852d377188f134e5a524549db055c9a1f2
AcOo0 <acoo.012@gmail.com>
2023-10-22 18:38:48 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const divBox = document.getElementById('boxes');
+const inputNumber = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroytBtn = document.querySelector('button[data-destroy]');
+
+createBtn.addEventListener('click', createBoxes);
+destroytBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) { 
+  amount = inputNumber.value;
+  for (let i = 0; i < amount; i++) {
+    const randomColor = getRandomHexColor();
+    const itemBox = document.createElement('div');
+    itemBox.style.backgroundColor = randomColor;
+    itemBox.style.width = `${30 + i * 10}px`;
+    itemBox.style.height = `${30 + i * 10}px`;
+    divBox.appendChild(itemBox);
+  }
+};
+
+function destroyBoxes() {
+  divBox.innerHTML = '';
+  inputNumber.value = '';
+}
