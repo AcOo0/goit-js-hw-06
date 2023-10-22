ad7e9095852e2c9227fe0e61b988177d5449f368
AcOo0 <acoo.012@gmail.com>
2023-10-22 17:35:54 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) { 
+    event.preventDefault();    
+    const {email, password} = event.currentTarget.elements;
+    
+    const user = {
+        email: email.value,
+        password: password.value,
+    };
+
+    if (email.value === '' || password.value === '') {
+        alert('Ой! Щось пішло не так! Ви точно заповнили всі поля?')
+    } else { 
+        console.log(user);
+        alert('Дякую. Віші дані прийняті. Залишайтесь на місці, за Вами вже виїхали.');
+    };
+
+    event.currentTarget.reset();
+};
